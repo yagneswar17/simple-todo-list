2974707b1b619de693bcffca06a1ef278b044e3c
yagneswar17 <yagneswar.naidu@gmail.com>
2022-05-22 10:20:10 +0530

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import "./Todo.css";
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isEditing: false, task: this.props.task };
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleRemove() {
+        this.props.removeTodo(this.props.id);
+    }
+    handleUpdate(evt) {
+        evt.preventDefault();
+        this.props.updateTodo(this.props.id, this.state.task);
+        this.setState({ isEditing: false });
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+    render() {
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <div className="Todo">
+                    <form onSubmit={this.handleUpdate}>
+                        <input type='text' value={this.state.task} name="task" onChange={this.handleChange} />
+                        <button>Save</button>
+                    </form>
+                </div>
+            );
+        }
+        else {
+            result = (
+                <div className="Todo">
+                    <button onClick={this.handleRemove}>X</button>
+                    <button onClick={() => this.setState({ isEditing: !this.state.isEditing })}>EDIT</button>
+                    <li className={this.props.completed ? "Todo completed" : "Todo task"} onClick={() => this.props.toggleTodo(this.props.id)} >
+                        {this.props.task}
+                    </li>
+                </div>
+            );
+        }
+        return result;
+    }
+}
+
+export default Todo;
